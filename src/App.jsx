339662369c29fd5ba3f4902a6982a9a6704bd17f
이채연 <196144872+codus69@users.noreply.github.com>
2026-07-03 12:01:09 +0900

    feat: 상세조회 및 라우팅 구현+오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router';
 import BoardList from './components/BoardList';
 import Write from './components/Write';
+import View from './components/View';
 
 function App() {
   return (
